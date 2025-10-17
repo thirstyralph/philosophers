e1a48e86af9052cae1ad87241b900cd91fa09036
thirstyralph <thirstyralph@protonmail.com>
2025-10-17 02:48:51 +0200

    fresh start

---
+++ 1/src/philo.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ranavarr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 23:33:37 by ranavarr          #+#    #+#             */
+/*   Updated: 2025/10/17 01:38:49 by ranavarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../philo.h"
+
+/*
+ * t_app innit
+ * parses the given arguments
+ * declares and alloc's the conf array
+ * declares and alloc's the t_app struct
+ * return:
+ * 	On success: t_app struct
+ * 	On failure: NULL;
+ */
+t_app	*init(int argc, char **argv)
+{
+	t_app	*app;
+	t_conf	*conf;
+
+	conf = parse(argc, argv);
+	if (!conf)
+		return (NULL);
+	app = malloc(sizeof(t_app));
+	if (!app)
+		return (NULL);
+}
+
+/*
+ * int	philo
+ * 1. parse, reads arguments and returns t_conf struct;
+ * 2. app gets declared and app.life_lock init;
+ * 3. spawn_forks, t_fork array gets alloc'd and init;
+ * 4. spawn_philos, for each and in alterning order:
+ * 		- t_philo array gets alloc'd
+ * 		- forks get asigned
+ * 		- meals_lock gets init
+ * 		- last_meal gets set to conf->start
+ * 5. the hapeningenging happens
+ * 6. t_fork array gets destroyed and free
+ * 7. print lock gets destroyed
+ * 8. meals lock gets destroyed
+ * 9. t_philo array gets freed
+ */
+int	philo(int argc, char **argv)
+{
+	t_app	*app;
+	t_fork	*forks;
+
+	app = init(argc, argv);
+	if (!app)
+		return (1);
+	free(app->conf);
+	free(app);
+	return (0);
+}
---
+++ 1/src/spawn_forks.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   spawn_forks.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ranavarr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/17 01:41:56 by ranavarr          #+#    #+#             */
+/*   Updated: 2025/10/17 02:25:20 by ranavarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../philo.h"
+
+/*
+ * pthread_mutex_t *spawn_forks
+ * allocs and inits an array of mutexes to be used as forks
+ * returns:
+ * on success: pthread_mutex_t array;
+ * on failure: NULL;
+ */
+pthread_mutex_t	*spawn_fork(unsigned int n)
+{
+	pthread_mutex_t	*forks;
+	int				i;
+
+	forks = malloc(sizeof(pthread_mutex_t) * n);
+	if (!forks)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		if (!pthread_mutex_init(&forks[i], NULL))
+		{
+			while (i < 0)
+			{
+				pthread_mutex_destroy(&forks[i]);
+				i--;
+			}
+			free(forks);
+			return (NULL);
+		}
+		i++;
+	}
+	return (forks);
+}
+
+int	destroy_and_free_forks(pthread_mutex_t *forks, unsigned long n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		pthread_mutex_destroy(&forks[i]);
+		i++;
+	}
+	free(forks);
+}
